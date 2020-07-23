75362628d1fc16af594c683a793cd0a5e146f4b0
Darryl Patterson <widgetapps@gmail.com>
2020-07-23 10:31:48 -0400

    Added files for Docker and Google Cloud Run deployment.

---
+++ server.js
@@ -0,0 +1,7 @@
+let express = require('express');
+let app = express();
+app.use(express.static('dist/iiot-platform-web'));
+app.get('/', function (req, res,next) {
+  res.redirect('/');
+});
+app.listen(8080);

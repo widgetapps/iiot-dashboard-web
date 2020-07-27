2f7252ead7febe6ae2efa51af3b9c32120970298
Darryl Patterson <widgetapps@gmail.com>
2020-07-26 23:51:28 -0400

    Activated React Dev Tools.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from "../environments/environment";
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +16,6 @@ import { EffectsModule } from '@ngrx/effects';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AuthEffects } from "./core/auth/login/store/effects";
-import  * as fromAuth from './store/auth.reducer';
 import { ROOT_REDUCERS } from './store';
 
 import { JwtInterceptor } from "./core/interceptors/jwt.interceptor";
@@ -33,7 +34,11 @@ import { JwtInterceptor } from "./core/interceptors/jwt.interceptor";
     MatSidenavModule,
     MatSnackBarModule,
     StoreModule.forRoot(ROOT_REDUCERS),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
